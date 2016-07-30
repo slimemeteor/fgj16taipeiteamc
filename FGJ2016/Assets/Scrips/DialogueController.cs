de4e02e79e8b478cfab5498b7236d4368ae217c2
IDer <roemastenku@gmail.com>
2016-07-30 16:32:04 +0800

    Dialogue Script and UGUI

---
+++ FGJ2016/Assets/Scrips/DialogueController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueController : MonoBehaviour {
+
+	private static DialogueController _instance ;
+
+	public static DialogueController Instance 
+	{
+		get
+		{
+			if(_instance == null)	_instance = Camera.main.gameObject.GetComponent<DialogueController>();
+			return _instance;
+		}
+	}
+
+	public GameObject DialogueUI;
+
+	public void SetDialogueUI(Transform uiPos , string dialogueContent)
+	{
+		DialogueUI.transform.SetParent(uiPos);
+		DialogueUI.transform.localPosition = Vector3.zero ;
+		DialogueUI.GetComponent<Dialogue>().SetDialogueContent(dialogueContent);
+		DialogueUI.SetActive(true);
+	}
+
+}
