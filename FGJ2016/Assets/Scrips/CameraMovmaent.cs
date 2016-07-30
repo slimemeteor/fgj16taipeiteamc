5c999838055dead99ae6e15f649b229a01d6ebca
張庭昇 <tim12332000@gmail.com>
2016-07-30 15:50:34 +0800

    first commit

---
+++ FGJ2016/Assets/Scrips/CameraMovmaent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovmaent : MonoBehaviour {
+
+	private GameObject player;
+	public Vector3 offset;
+
+	void Awake(){
+	
+		player = GameObject.Find("Player");
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ FGJ2016/Assets/Scrips/Player.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+class PlayerTouchEventArg : EventArgs
+{
+	
+}
+
+public class Player : MonoBehaviour {
+
+	Animator anim ;
+	public float Speed;
+
+	private EventHandler _turnSoil ;
+	private EventHandler _watering ;
+	private EventHandler _farming ;
+	public event EventHandler TurnSoil { add {	_turnSoil += value; } remove { _turnSoil -= value; } }
+	public event EventHandler Watering { add {	_watering += value; } remove { _watering -= value; } } 
+	public event EventHandler Farming { add { _farming += value; } remove { _farming -= value; } }
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var pos = transform.position;
+
+
+		float input_x = Input.GetAxis ("Horizontal")*Speed;
+		this.OnMove (input_x );
+			
+	}
+		
+	private void OnMove(float input_x ){
+		
+		bool isWalking = Mathf.Abs (input_x) > 0; 
+		anim.SetBool ("isWalking", isWalking);
+
+		if (isWalking) {
+			anim.SetFloat ("x", input_x);
+
+			transform.position += new Vector3 (input_x, 0 , 0)* Time.deltaTime;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "dog") {
+		
+		}
+
+		if (other.tag == "Field") {
+			
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other){
+		
+	}
+}
